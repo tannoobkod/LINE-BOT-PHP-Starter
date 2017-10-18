c1e76cd5dd77a5afed9d4733c66286ed3ff24b33
tannoobkod <32521356+tannoobkod@users.noreply.github.com>
2017-10-18 19:47:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,24 +27,7 @@ if (!is_null($events['events'])) {
 				'text' => 'หมูฝอย'
 			];
 			}
-			if (is_numeric($text) && $pork == 1){
-				$pork_amount = 1;
-				$pork = 0;
-				$messages = [
-				'type' => 'text',
-				'text' => $text+'กล่อง'
-				$pork_amount =0;
-			];
-			}
-			if ( $text == 'หมูฝอย' && $menu == 1){
-				$menu = 0
-				$pork = 1;
-				$messages = [
-				'type' => 'text',
-				'text' => $pork
-				
-			];
-			}
+
 			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
