6f36fe89660dc4c38cd5134b80c4c694bc1acad2
tannoobkod <32521356+tannoobkod@users.noreply.github.com>
2017-10-18 19:06:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,11 +17,14 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			if ( $text == 'สวัสดี'){
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'เออ'
 			];
 
+			}
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
